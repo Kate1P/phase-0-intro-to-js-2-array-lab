51ae5f12808915ff00f59238cfd2b696079b1ba4
Kate Popova <popova1ea@gmail.com>
2022-11-27 14:47:07 -0800

    commit tree 0570bbd4b7ca8490bbc122afaeaecf3894a6cf26

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(){
+const copyOfCats = [...cats, "Broom"];
+return copyOfCats;
+}
+
+function prependCat(name){
+    const copyOfCats = ["Arnold", ...cats];
+    return copyOfCats;
+    }
+
+function removeLastCat(){
+    const copyOfCats = [...cats.slice(0,2)];
+    return copyOfCats;
+    }   
+
+function removeFirstCat(){
+    const copyOfCats = [...cats.slice(1)];
+    return copyOfCats;
+    }  
